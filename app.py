40130ac4ad99559288e57aba8a4aea2d662fe5aa
Vineeth Ramesh <67580974+vineeth2281@users.noreply.github.com>
2025-04-13 12:07:52 +1000

    Update app.py

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def get_s3_client():
 
 # Database Configuration, REPLACE with your RDS credentials
 DB_HOST = "vram0324-caption-db.c3wo6q26qzc8.us-east-1.rds.amazonaws.com"
-DB_NAME = "image_caption_db"
+DB_NAME = "vram0324-caption-db"
 DB_USER = "admin"
 DB_PASSWORD = "Rvineeth23"
 
