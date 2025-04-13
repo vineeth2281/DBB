e31147a473b9d493e15d6607329f46a5beae0a3c
Vineeth Ramesh <67580974+vineeth2281@users.noreply.github.com>
2025-04-13 16:18:27 +1000

    Update create-database.sh

--- create-database.sh
+++ create-database.sh
@@ -3,7 +3,7 @@
 # RDS connection details
 DB_HOST="vram0324-caption-db.c3wo6q26qzc8.us-east-1.rds.amazonaws.com"           # Replace with your RDS endpoint
 DB_USER="admin"           # Yur RDS admin username
-DB_PASSWORD="Rvineeth23"       # Your RDS password
+DB_PASSWORD="Rvineeth24"       # Your RDS password
 SQL_COMMANDS=$(cat <<EOF
 /*
   Database Creation Script for the Image Captioning App
